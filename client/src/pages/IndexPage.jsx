eb672264338d2dcd5c2caddf836fc867c49b5216
titustuss <gichukituss@gmail.com>
2023-07-28 16:14:42 +0300

    fix: image display

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -16,7 +16,7 @@ return (
       <Link to={'/place/' + place._id} >
         <div className="bg-gray-500 mb-2 rounded-2xl flex">
         {place.photos?.[0] && (
-          <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="" />
+          <img className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt="" />
         )}
         </div>
         <h2 className="text-xl truncate ">{place.title}</h2>
