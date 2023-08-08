8ae04d1a274896a9dff2eafee6886588be5bf814
trevor <trevorwendo@gmail.com>
2023-08-08 04:36:57 +0300

    Refactor: Property Sharing Among Components

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
-export default function Layout()
+export default function Layout({category, setCategoryValue})
 {
     return(
         <div className="py-4 px-10 flex flex-col min-h-screen">
-        <Header/>
+        <Header category={category} setCategoryValue={setCategoryValue}/>
         <Outlet/>
         </div>
     );
