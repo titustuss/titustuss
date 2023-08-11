7841cc5c9f0f93bb83ce3bff4f39ea1ceac08845
trevor <trevorwendo@gmail.com>
2023-08-11 09:59:55 +0300

    build: Server running script

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to the 'client' sub-folder
+cd client
+
+# Run 'npm run dev' in the 'client' sub-folder
+npm run dev &
+
+# Navigate back to the 'app' folder
+cd ..
+
+# Navigate to the 'api' sub-folder
+cd api
+
+# Run 'nodemon' in the 'api' sub-folder
+nodemon
+
