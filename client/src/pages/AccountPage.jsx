d82da1d9f6c55fc2128ab947990a4fd481d94ae3
titustuss <gichukituss@gmail.com>
2023-08-12 14:47:13 +0300

    feat: subscription

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -36,9 +36,82 @@ async function logout(){
             <h2>Hi!! { user.name}</h2>
           <AccountNav/>
             {subpage==='profile'&& (
-                <div className="text-center max-w-lg mx-auto">
-                    Logged in as {user.name} ({user.email}),<br/>
-                    <button onClick ={logout}className="primary max-w-sm mt-4">logout</button>
+
+                <div >
+                    <div className="grid sm:grid-flow-col justify-center gap-4">
+                        <div className="bg-white rounded overflow-hidden shadow-lg hover:shadow-blue-800/60 transition duration-300 relative">
+                            <div className="p-6 text-gray-600">
+                            <h1 className="text-2xl font-bold mb-2">BASIC PLAN</h1>
+                            <h1 className="text-4xl font-bold">$10/MO</h1>
+                            <p className="mt-4 mb-10">
+                                <span className="flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                                </svg>
+                                Add 1 property <br />
+                                add 8 photos <br />
+                                easy update <br />
+                                save 20% <br />
+                                </span>
+                            </p>
+                            </div>
+                            <button className="absolute rounded-full bottom-3 left-0 right-0 mx-auto w-full py-3 bg-white text-secondary font-semibold uppercase tracking-wide border-2 border-secondary ">
+                            START NOW
+                            </button>
+                        </div>
+
+                        <div className="bg-white rounded overflow-hidden shadow-lg hover:shadow-blue-800/60 transition duration-300 relative">
+                            <div className="p-6 text-gray-600">
+                                <h1 className="text-2xl font-bold mb-2">PREMIUM PLAN</h1>
+                                <h1 className="text-4xl font-bold">$100/YR</h1>
+                                <p className="mt-4 mb-10">
+                                    <span className="flex items-center gap-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                                        </svg>
+                                        Add 2 properties <br />
+                                        add 8 photos <br />
+                                        easy update <br />
+                                        save 20% <br />
+                                        Free Youtube video <br />
+                                        24 hrs support <br />
+                                    </span>
+                                </p><br />
+                            </div>
+                            <button className="absolute rounded-full bottom-3 left-0 right-0 mx-auto w-full py-3 bg-white text-secondary font-semibold uppercase tracking-wide border-2 border-secondary">
+                                START NOW
+                            </button>
+                            </div>
+
+                            <div className="bg-white rounded overflow-hidden shadow-lg hover:shadow-blue-800/60 transition duration-300 relative">
+                                <div className="p-6 text-gray-600">
+                                    <h1 className="text-2xl text font-bold mb-2">GOLDEN PLAN</h1>
+                                    <h1 className="text-4xl font-bold">$130/YR</h1>
+                                    <p className="mt-4 mb-10">
+                                        <span className="flex items-center gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                                            </svg>
+                                            Add 2 properties <br />
+                                            add 8 photos <br />
+                                            easy update <br />
+                                            save 20% <br />
+                                            Free Youtube video <br />
+                                            24 hrs support <br />
+                                        </span>
+                                    </p><br />
+                                </div>
+                                <button className="absolute rounded-full bottom-3 left-0 right-0 mx-auto w-full py-3 bg-white text-secondary font-semibold uppercase tracking-wide border-2 border-secondary  ">
+                                    START NOW
+                                </button>
+                            </div>
+
+                    </div>
+
+                    <div className="text-center max-w-lg mx-auto mt-10">
+                        Logged in as {user.name} ({user.email}),<br/>
+                        <button onClick ={logout}className="primary max-w-sm mt-4">logout</button>
+                    </div>
                 </div>
             )}
 
