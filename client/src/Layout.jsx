f807a9c03ddef54c2ad2cb304568bbf5fa3fe6fd
titustuss <gichukituss@gmail.com>
2023-08-12 16:06:19 +0300

    fix: merging error

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
-export default function Layout()
+export default function Layout({category, setCategoryValue})
 {
     return(
         <div className="py-5 px-6 sm:px-12 flex flex-col min-h-screen">
-        <Header/>
+        <Header category={category} setCategoryValue={setCategoryValue}/>
         <Outlet/>
         </div>
     );
